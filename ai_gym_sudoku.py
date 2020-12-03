23aafbd417e94bb321665dfbb8e2cf2e7e76b0b6
Stephen He <yihehe@gmail.com>
2020-12-02 21:43:31 -0500

    state = [grid,row,col,time]

--- ai_gym_sudoku.py
+++ ai_gym_sudoku.py
@@ -19,6 +19,7 @@ from keras import backend as K
 
 np.set_printoptions(precision=2)
 DEBUG = False
+TEST = True
 
 def mapping_to_target_range(x, target_min=0, target_max=9) :
     x02 = K.tanh(x) + 1 # x in range(0,2)
@@ -32,6 +33,7 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
+        self.good_memory = deque(maxlen=2000)
         self.gamma = 0.95    # discount rate
         self.epsilon = 0.90  # exploration rate
         self.epsilon_min = 0.01
@@ -60,7 +62,7 @@ class DQNAgent:
         model = Sequential()
         model.add(Dense(24, input_dim=self.state_size, activation='relu'))
         model.add(Dense(24, activation='relu'))
-        model.add(Dense(self.action_size, activation=mapping_to_target_range))
+        model.add(Dense(self.action_size))
         model.compile(loss=self._huber_loss,
                       optimizer=Adam(lr=self.learning_rate))
         return model
@@ -70,55 +72,78 @@ class DQNAgent:
         self.target_model.set_weights(self.model.get_weights())
 
     def memorize(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+        # if reward > 0:
+        #     self.good_memory.append((state, action, reward, next_state, done))
+        # else:
+            self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            row = random.randrange(9)
-            col = random.randrange(9) 
-            val = random.randrange(9)
-            if DEBUG: print('DEBUG: Random action! %s' % str([row, col, val]))
-            return row, col, val
+        if np.random.rand() <= self.epsilon and not TEST:
+            action = random.randrange(self.action_size)
+            if DEBUG: print('DEBUG: Random action', action)
+            return action
+        else:
+            qsa = self.model.predict(tf.constant([state]))[0]
+            if DEBUG: print('DEBUG: qsa', qsa)
+            action = np.argmax(qsa)
+            if DEBUG: print('DEBUG: action', action)
+            return action
+
+        # print('checking options')
+        # qsa = []
+        # for i in range(self.action_size):
+        #     next_state, _, _, _ = env.step(i)
+        #     qsa.append(self.model.predict(np.array([next_state]))[0][0])
+        # print(qsa)
+        # action = np.argmax(qsa)
+        # print('chosen action:', action)
+        # print('done checking')
+
+        # return action
+        # # input()
+
+        # if np.random.rand() <= self.epsilon:
+        #     row = random.randrange(9)
+        #     col = random.randrange(9) 
+        #     val = random.randrange(9)
+        #     if DEBUG: print('DEBUG: Random action! %s' % str([row, col, val]))
+        #     return row, col, val
         
-        act_values = self.model.predict(np.array([state.reshape(-1)]))  
-        row = math.floor(act_values[0][0])
-        col = math.floor(act_values[0][1])
-        val = math.floor(act_values[0][2])
+        # act_values = self.model.predict(np.array([state.reshape(-1)]))  
+        # row = math.floor(act_values[0][0])
+        # col = math.floor(act_values[0][1])
+        # val = math.floor(act_values[0][2])
         
-        if DEBUG: print('DEBUG: Agent Action! %s' % str([row, col, val]))
-        return row, col, val
+        # if DEBUG: print('DEBUG: Agent Action! %s' % str([row, col, val]))
+        # return row, col, val
 
     def replay(self, batch_size):
-        minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            np_state = np.array([state.reshape(-1)])
-            np_next_state = np.array([next_state.reshape(-1)])
-            
-            target = reward
-            if not done:
-                target = (reward + self.gamma *
-                          np.amax(self.model.predict(np_next_state)[0]))
-                
+        minibatch = random.sample(self.memory, int(batch_size / 2))
+        # good_minibatch = random.sample(self.good_memory, min(int(batch_size / 2), len(self.good_memory)))
+        x = []
+        y = []
+        for state, action, reward, next_state, done in minibatch:# + good_minibatch:
+            qsa_state = self.model.predict(tf.constant([state]))[0]
+            qsa_next_state = self.model.predict(tf.constant([next_state]))[0]
 
-            target_f = self.model.predict(np_state)
-            
-            # print('Action:      %s' % str(action))
-            # print('Reward:      %s' % str(reward))
-            # print('Target:      %s' % str(target))
-            # print('Target_f:    %s' % str(target_f[0]))
-            
-            # Sketchy solution that might not work, but I don't know what else to do:
-            # Target_f: Future discounted reward
-            # We want the agent to map the current state to future discounted reward 
-            # Because we have 3 different actions, I will randomly assign one of the values to the new target array, 
-            # I will assign all 3 values to the target array. 
-            
-            # This is the key line that I have to figure out
-            target_f[0][random.randint(0,2)] = target
-            
-            # Old code that I don't really understand 
-            # target_f[0][action] = target
-            self.model.fit(np_state, target_f, epochs=1, verbose=0)
+            # print(qsa_state)
+            # print(qsa_next_state)
+
+            target = qsa_state
+            target[action] = reward
+            if not done:
+                target[action] += self.gamma * np.amax(qsa_next_state)
+
+            if DEBUG: print('target', target)
+            # if DEBUG: input("Pausing...")
+            x.append(state)
+            y.append(target)
+
+        # print(len(good_minibatch))
+        # print(len(x))
+
+        with tf.device('/gpu:0'):
+            self.model.fit(tf.constant(x), tf.constant(y), epochs=1, verbose=0)
             
             
         if self.epsilon > self.epsilon_min:
@@ -145,19 +170,37 @@ env = gym.make('Sudoku-v0')
 env.base = puzzles[0][0] # Unfinished puzzle
 env.sol = puzzles[0][1]  # Solution (used to inform reward function)
 
-state_size = 81
-action_size = 3
+state_size = 81 + 3
+action_size = 9 + 4
 agent = DQNAgent(state_size, action_size)
 done=False
 batch_size=64
 
 best_score = None
+# DEBUG = True
+# sudoku_env.setDebug(DEBUG)
+
+if TEST:
+    DEBUG = True
+    sudoku_env.setDebug(DEBUG)
+
+try:
+    agent.load('last_trained_agent')
+except Exception as e:
+    print(e)
 
 for i in range(num_episodes):
+
+    puz = random.randrange(len(puzzles))
+    env.base = puzzles[puz][0] # Unfinished puzzle
+    env.sol = puzzles[puz][1]  # Solution (used to inform reward function)
     
     state = env.reset()
     score = 0
-        
+    # if i % 20 == 0:
+    #     DEBUG = not DEBUG
+    #     sudoku_env.setDebug(DEBUG)
+
     for t in range(200):
     
         if DEBUG: env.render()
@@ -166,6 +209,7 @@ for i in range(num_episodes):
         action = agent.act(state)
                 
         next_state, reward, done, _ = env.step(action) # take a random action
+        if DEBUG: print('reward %d' % reward)
         score += reward
         # next_state = step_res[0] # An environment specific object representing our observation (e.g. Pixel data from Camera, Joint Angles, Joint velocities, Board state) 
         # reward = step_res[1] # Amount of reward achieved by previous action 
@@ -191,16 +235,20 @@ for i in range(num_episodes):
             agent.update_target_model()
             print("episode: {}/{}, score: {}, e: {:.2}".format(i, num_episodes, t, agent.epsilon))
             break
-    
+
     if (best_score is None):
         best_score = score
     else: 
         if (score > best_score):
             best_score = score
     
-    print('Episode: %d, score: %d, best_score: %d, epsilon: %0.3f'% (i,score, best_score, agent.epsilon))
+    print('Episode: %d, score: %d, best_score: %d, epsilon: %0.3f, unfilled: %d'% (i,score, best_score, agent.epsilon, (env.grid == 0).sum()))
     
     agent.replay(batch_size)
+    DEBUG = False
+    sudoku_env.setDebug(DEBUG)
+
+    agent.save('last_trained_agent')
        
         # act_row = num_let_map[env.last_action[0]]
         # act_col = env.last_action[1] + 1 # We add 1 here because grid columns are visualized from 1-9
@@ -218,4 +266,4 @@ for i in range(num_episodes):
         # input('Press enter to continue!')
     
 env.close()
-DQNAgent.save('last_trained_agent')
+agent.save('last_trained_agent')
--- sudoku_env.py
+++ sudoku_env.py
@@ -6,6 +6,11 @@ from termcolor import colored, cprint
 
 from gym.envs.registration import register
 
+DEBUG = False
+def setDebug(d):
+	global DEBUG
+	DEBUG = d
+
 register(
     id='Sudoku-v0',
     entry_point=__name__ + ':SudokuEnv',
@@ -37,11 +42,11 @@ def check_legal_action(val, row, col, array):
     # print('Val: %s' %str(val))
     
     if (val in array[row]):
-        print('Value exists in row!')
+        if DEBUG: print('Value exists in row!')
         return False 
     
     if (val in array[:,col]):
-        print('Value exists in column!')
+        if DEBUG: print('Value exists in column!')
         return False 
     
     # To check subgrid, I first need to find the correct subgrid
@@ -49,7 +54,7 @@ def check_legal_action(val, row, col, array):
     col_cor = 3*int(col/3)
     
     if (val in array[row_cor:row_cor + 3, col_cor:col_cor + 3]):
-        print('Value exists in subgrid!')
+        if DEBUG: print('Value exists in subgrid!')
         return False 
     
     return True 
@@ -146,27 +151,95 @@ class SudokuEnv(gym.Env):
 	#   - a reward: - negative if action leads to an error
 	#               - positive if action is correct or grid is resolved
 	def step(self, action):
-        
-        # Action is sampled based on the function in __init__. 
+		# Action is sampled based on the function in __init__. 
 		self.last_action = action
-		oldGrid = np.copy(self.grid)
-        
-		# The user can't replace a value that was already set
-		if self.grid[action[0], action[1]] != 0:
-            # Setting reward to -10 to more strongly penalize 
-            # the algorithm when it tries to overwrite existing values
-			print('Tried to overwrite existing value! Reward: %d' % (-10))
-			return np.copy(self.grid), -10, False, None
 
+		# action[0-13]
+		r_impossible = -5
+		r_illegal_movement = -5
+		r_correct = 5
+		r_finish = 10
+		# r_first_move = 10
+		r_good_effort = -1
+
+		# r_movement should range from 2 -> -2 # https://www.desmos.com/calculator/kn9tpwdan5
+		a = 5; b = -0.5; k = 4
+		sigmoid = k / (1 + np.exp(a + b * self.agent_state[2]))
+		if DEBUG: print(self.agent_state[2], sigmoid)
+		r_movement = 2 - sigmoid
+
+		# decrement time value every step
+		self.agent_state[2] += 1
+
+		if action == 9: # up
+			is_legal = self.agent_state[0] > 0
+			if is_legal:
+				if DEBUG: print('action: %s moving up' % (action))
+				self.agent_state[0] -= 1
+				return self.state(), r_movement, False, None
+			else:
+				if DEBUG: print('action: %s illegal movement' % (action))
+				return self.state(), r_illegal_movement, False, None
+		if action == 10: # down
+			is_legal = self.agent_state[0] < 8
+			if is_legal:
+				if DEBUG: print('action: %s moving down' % (action))
+				self.agent_state[0] += 1
+				return self.state(), r_movement, False, None
+			else:
+				if DEBUG: print('action: %s illegal movement' % (action))
+				return self.state(), r_illegal_movement, False, None
+		if action == 11: # left
+			is_legal = self.agent_state[1] > 0
+			if is_legal:
+				if DEBUG: print('action: %s moving left' % (action))
+				self.agent_state[1] -= 1
+				return self.state(), r_movement, False, None
+			else:
+				if DEBUG: print('action: %s illegal movement' % (action))
+				return self.state(), r_illegal_movement, False, None
+		if action == 12: # right
+			is_legal = self.agent_state[1] < 8
+			if is_legal:
+				if DEBUG: print('action: %s moving right' % (action))
+				self.agent_state[1] += 1
+				return self.state(), r_movement, False, None
+			else:
+				if DEBUG: print('action: %s illegal movement' % (action))
+				return self.state(), r_illegal_movement, False, None
+
+		if self.grid[self.agent_state[0], self.agent_state[1]] != 0:
+			# print('Tried to overwrite existing value! Reward: %d' % (-10))
+			if DEBUG: print('action: %s overwrite' % (action))
+			return self.state(), r_impossible, False, None
+        
+        
 
         # legal_reward: 1,   correct_reward:  2,    finished_reward:    10
-		is_legal = check_legal_action(action[2]+1, action[0], action[1], self.grid)
-		is_correct = self.sol[action[0], action[1]] == (action[2] + 1)
-		is_finished = check_solution_manual(self.grid)       
+		is_correct = self.sol[self.agent_state[0], self.agent_state[1]] == (action + 1)
+		if is_correct:
+			# We add one to the action because the action space is from 0-8 and we want a value in 1-9
+			self.grid[self.agent_state[0], self.agent_state[1]] = action+1
+
+			is_finished = check_solution_manual(self.grid)
+			if is_finished:
+				if DEBUG: print('action: %s finish' % (action))
+				return self.state(), r_finish, True, None
+			else:
+				if DEBUG: print('action: %s correct' % (action))
+				self.agent_state[2] = 0 # reset after correct move
+				return self.state(), r_correct, False, None
+		is_legal = check_legal_action(action+1, self.agent_state[0], self.agent_state[1], self.grid)
+		if is_legal:
+			# print('Tried to write the wrong value! Reward: %d' % (0))
+			if DEBUG: print('action: %s wrong value' % (action))
+			return self.state(), r_good_effort, False, None
+		else:
+			if DEBUG: print('action: %s illegal' % (action))
+			return self.state(), r_illegal_movement, False, None
 
 		# We add one to the action because the action space is from 0-8 and we want a value in 1-9
-		self.grid[action[0], action[1]] = action[2]+1
-
+		# self.grid[self.agent_state[0], self.agent_state[1]] = action+1
     
         # is_legal: Check if the current move is legal given the state of the board
         # is_correct: Checks if the current move matches the solution of the puzzle
@@ -176,16 +249,16 @@ class SudokuEnv(gym.Env):
 		
         # Reward functions based on correctness and legality
 		# If grid is complete or correct, return positive reward
-		if is_finished: 
-			return np.copy(self.grid), 10, True, None
-		if is_correct:
-			return np.copy(self.grid), 2, False, None
-		if is_legal: # If it is unfinished but legal 
-			return np.copy(self.grid), 1, False, None
-		else:
-			# If move is wrong, return to old state, and return negative reward
-			self.grid = oldGrid
-			return np.copy(self.grid), -1, False, None
+		# if is_finished: 
+		# 	return np.copy(self.grid), 10, True, None
+		# if is_correct:
+		# 	return np.copy(self.grid), 2, False, None
+		# if is_legal: # If it is unfinished but legal 
+		# 	return np.copy(self.grid), 1, False, None
+		# else:
+		# 	# If move is wrong, return to old state, and return negative reward
+		# 	self.grid = oldGrid
+		# 	return np.copy(self.grid), -1, False, None
         
         # Original Reward Functions 
 		# stats = checkSolution(self.grid)
@@ -205,8 +278,12 @@ class SudokuEnv(gym.Env):
 	def reset(self):
 		self.last_action = None
 		self.grid = np.copy(self.base)
-		return np.copy(self.grid)
+		self.agent_state = np.array([4,4,0]) # row, col, steps since last number
+		return self.state()
+		
 
+	def state(self):
+		return np.concatenate((np.copy(self.grid).reshape(-1), np.copy(self.agent_state)))
 
 	def render(self, mode='human', close=False):
 		### This basically just prints out the game board, and is supposed to highlight 
@@ -219,7 +296,7 @@ class SudokuEnv(gym.Env):
 		mid_row = '  ├─────────┼─────────┼─────────┤'
 		bot_row = '  └─────────┴─────────┴─────────┘'
 		
-		last_action = self.last_action
+		# last_action = self.last_action
 		array = np.where(self.grid == 0, np.nan, self.grid)
 		num_to_let = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
@@ -235,7 +312,10 @@ class SudokuEnv(gym.Env):
 				else:
 					cell_val = int(cell_val)
 
-				row_string += ' ' + str(cell_val) + ' ' 
+				if i == self.agent_state[0] and j == self.agent_state[1]:
+					row_string += '[' + str(cell_val) + ']' 
+				else:
+					row_string += ' ' + str(cell_val) + ' ' 
 				if (j == 2 or j == 5 or j == 8):
 					row_string += '|'
 			print(row_string)
