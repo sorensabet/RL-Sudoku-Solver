762c5f50a2ed93e21f0c07639239de27324219bc
alvin <alvin_heng95@hotmail.com>
2020-12-15 01:49:10 +0800

    final 729_actions model

--- ai_gym_sudoku.py
+++ ai_gym_sudoku.py
@@ -1,229 +1,224 @@
-# import numpy as np
-# import pandas as pd 
-import sys
-import gym
-import gym_sudoku
-import numpy as np 
-import helper
-import soren_solver
-import time
-import math
-import random
-import numpy as np
+from tensorflow import keras
 import tensorflow as tf
 from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
-from keras import backend as K
+import random
+import gym
+import sudoku_env
+import numpy as np
+import my_keras_agent
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# Deep Q-learning Agent
-# Code for DQQ from: https://github.com/keon/deep-q-learning/blob/master/ddqn.py
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=4000)
-        self.gamma = 0.95    # discount rate
-        self.epsilon = 0.90  # exploration rate
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.99
-        self.learning_rate = 0.001
-        self.model = self._build_model()
-        self.target_model = self._build_model()
-        self.update_target_model()
 
-    """Huber loss for Q Learning
-    References: https://en.wikipedia.org/wiki/Huber_loss
-                https://www.tensorflow.org/api_docs/python/tf/losses/huber_loss
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+def arr_index_to_action(index):
     """
-
-    def _huber_loss(self, y_true, y_pred, clip_delta=1.0):
-        error = y_true - y_pred
-        cond  = K.abs(error) <= clip_delta
-
-        squared_loss = 0.5 * K.square(error)
-        quadratic_loss = 0.5 * K.square(clip_delta) + clip_delta * (K.abs(error) - clip_delta)
-
-        return K.mean(tf.where(cond, squared_loss, quadratic_loss))
-
-    def _build_model(self):
-        # Neural Net for Deep-Q learning Model
-        model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(24, activation='relu'))
-        model.add(Dense(self.action_size, activation='softmax'))
-        model.compile(loss=self._huber_loss,
-                      optimizer=Adam(lr=self.learning_rate))
-        return model
-
-    def update_target_model(self):
-        # copy weights from model to target_model
-        self.target_model.set_weights(self.model.get_weights())
-
-    def memorize(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            rand_act = mapping[np.random.randint(0,729)]
-            row = rand_act[0]
-            col = rand_act[1]
-            val = rand_act[2]  
-            return row, col, val
-        
-        act_values = mapping[np.argmax(self.model.predict(np.array([state.reshape(-1)])))]
-        row = act_values[0]
-        col = act_values[0]
-        val = act_values[0]
-        
-        #print('Agent Action! %s' % str(act_values))
-        return row, col, val
+    Maps an index 0-728 to an action: eg, put 3 in row 5, col 2
+    The index will be the output of the NN's predict function because it takes an
+    argmax of 729 layers
+    """
     
-    # DDQN Code
-    def replay(self, batch_size):
-        print('Fitting model!')
+    num_to_fill = index//81 + 1 # +1 as numbers are 1-9, not 0-8
+    
+    flattened_index = index - index//81 * 81
+    row = flattened_index//9
+    col = flattened_index - row * 9
+    
+    return [row, col, num_to_fill]
 
-        minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            np_state = tf.convert_to_tensor(np.array([state.reshape(-1)]))
-            np_next_state = tf.convert_to_tensor(np.array([next_state.reshape(-1)]))
-            
-            target = self.model.predict(np_state)
-            action_idx = np.where((mapping  == action).all(axis=1))
+def get_init_states_index(grid):
+    """
+    Given a starting sudoku board (2d numpy array), get the indices of the grids that are non-zero.
+    You want to heavily penalize these grids because the agent is not supposed to change it at all
+    """
+    m = np.nonzero(grid)
+    return list(zip(m[0],m[1]))
 
-            if done: 
-                target[0][action_idx] = reward
-            else:
-                t = self.target_model.predict(np_next_state)[0]
-                target[0][action_idx] = reward + self.gamma * np.amax(t)
-            
-            self.model.fit(np_state, target, epochs=1, verbose=0)
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+def correct_boxes(init_state, final_state, sol):
+    """
+    Given the initial state, final state and solution, calculate the number of boxes the agent got corret
+    at the end of an episode
+    """
+    num_init = np.count_nonzero(init_state)
+    num_same = np.equal(final_state,sol).sum()
+    
+    return num_same - num_init #subtract out the numbers given at the start of the puzzle
 
-    def load(self, name):
-        self.model.load_weights(name)
+load = True # indicate if you want to load an existing model
+# model is saved in folder /my_model
 
-    def save(self, name):
-        self.model.save_weights(name)
-
-
-
-# Load data
-puzzles = np.load('puzzles.npy')
-mapping = np.load('729_mapping.npy')
-num_let_map = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-curr_puzz = puzzles[0][0]
-
-# Model parameters
-num_episodes = 10000
-
-# Initialize environment
 env = gym.make('Sudoku-v0')
-env.base = puzzles[0][0] # Unfinished puzzle
-env.sol = puzzles[0][1]  # Solution (used to inform reward function)
+puzzles = np.load('puzzles.npy')
+agent = my_keras_agent.SudokuAgent()
 
-state_size = 81
-action_size = len(mapping)
-agent = DQNAgent(state_size, action_size)
-done=False
-batch_size=64
-
-best_score = None
-
-puzz_num = 0
-for episode in range(num_episodes):
-        
-    state = env.reset()
-    score = 0
-
-    start_grid = env.grid
-    is_done = False
-
-    for time_step in range(500):
+# This is how we pass in the puzzle to be solved
+if load:
+    params = np.loadtxt('agent_data.txt')
+    agent.epsilon = params[0]
+    puzz_counter = int(params[1]) + 1
+    agent.model = keras.models.load_model("my_model")
     
-        
-        # Decide action
-        action = agent.act(state)
-            
-        next_state, reward, done, _ = env.step(action) # take a random action
-        score += reward
-        
-        # I need to check if the puzzle is finished (if next_state is complete)
-        # So that if it is complete, the puzzle doesn't keep going on endlessly
-        is_done = helper.check_solution_auto(env.grid, env.sol)
-        if (is_done is True):
-            break
-        
-        # I'm also going to put a check if the puzzle is still solvable after a legal move is made 
-        # If the puzzle is not solvable, it should be penalized very heavily and the episode should end
-        
-        #print(next_state)
-        #input('Batman')
-        
-        # next_state = step_res[0] # An environment specific object representing our observation (e.g. Pixel data from Camera, Joint Angles, Joint velocities, Board state) 
-        # reward = step_res[1] # Amount of reward achieved by previous action 
-        # done = step_res[2] # Whether it is time to reset the environment
-        # info   = step_res[3] # Diagnostic info for debugging
-        
-        agent.memorize(state, action, reward, next_state, done)
-        state = next_state
-        
-        print('\tEpisode: %d-%d, score: %s, best_score: %s'% (episode, time_step, str(score), str(best_score)))
-        
-        act_row = num_let_map[action[0]-1]
-        act_col = action[1] 
-        act_val = action[2] 
-            
-        #env.render()
-        # print('Last action: [%s,%d] ==> %d \n\n\n\n\n' % (act_row, act_col, act_val))
-        # input('Batman ')
-        
+    avg_reward_array = np.load('avg_reward.npy')
+    correct_boxes_array = np.load('num_corr_boxes.npy')
+    
+
+else:
+    puzz_counter = 0
+
+env.base = puzzles[puzz_counter][0] # Unfinished puzzle
+env.sol = puzzles[puzz_counter][1]  # Solution (used to inform reward function)
+
+
+# training loop
+
+if train is True:
+    
+    episodes = 1000
+    done = True
+    avg_reward_array = []
+    correct_boxes_array = []
+
+    # Iterate the game
+    for e in range(episodes):
+
         if done:
-            agent.update_target_model()
-            print("episode: {}/{}, score: {}, e: {:.2}".format(episode, num_episodes, time_step, agent.epsilon))
-            break
-    
-    if (best_score is None):
-        best_score = score
-    else: 
-        if (score > best_score):
-            best_score = score
-    
-    print('Episode: %d, score: %d, best_score: %d, epsilon: %0.3f'% (episode,score, best_score, agent.epsilon))
-    env.render()
-    
-    
-    if (is_done is True): 
-        print('COMPLETED A PUZZLE CORRECTLY!')
-        with open('logs.txt', 'a') as file:
-            file.write('Completed puzzle ' + str(puzz_num) + ' correctly!')
-        
-        puzz_num += 1
-        env.base = puzzles[puzz_num][0] # Unfinished puzzle
-        env.sol = puzzles[puzz_num][1] 
-    
-    if (start_grid == env.grid).all():
-        print('No changes detected in episode, changing game board!')
-        with open('logs.txt', 'a') as file:
-            file.write('Could not solve puzzle ' + str(puzz_num) + ' correctly.')
-        
-        puzz_num += 1
-        env.base = puzzles[puzz_num][0] # Unfinished puzzle
-        env.sol = puzzles[puzz_num][1] 
-    
-    agent.replay(batch_size)
-       
+            init_state = puzzles[puzz_counter][0] # Unfinished puzzle
+            solution = puzzles[puzz_counter][1] # Solution
+            env.base = np.copy(init_state)
+            env.sol = np.copy(solution)
+            init_states_index = get_init_states_index(puzzles[puzz_counter][0])
+            env.init_states_index = init_states_index
+            puzz_counter += 1
 
-    
-        # Reward of 1 indicates legal move, reward of 2 indicates correct move
-        #if (step_res[1] == 1 or step_res[1] == 2):
-        # print('Time Step: %d, Reward: %d' % (i, step_res[1]))
-        # input('Press enter to continue!')    
-        # else: 
-        #     print('Time Step: %d, Reward: %d' % (i, step_res[1]))
-        # print('\n\n\n')
-        # input('Press enter to continue!')
-    
-env.close()
+        state = env.reset()
+        reward_for_ep = 0
+
+        for num_iters in range(1,10001):
+
+            # turn this on if you want to render
+            # env.render()
+
+            # Decide action
+            action_index_flat = agent.act(state.reshape(1,-1))
+            action = arr_index_to_action(action_index_flat)
+
+            # Advance the game
+            next_state, reward, done, _ = env.step(action)
+            reward_for_ep += reward
+
+            # memorize the previous state, action, reward, and done
+            agent.memorize(state.reshape(1,-1), action_index_flat, reward, next_state.reshape(1,-1), done)
+
+            if done:
+                break
+
+            # make next_state the new current state for the next frame.
+            state = np.copy(next_state)
+
+        avg_reward = reward_for_ep/num_iters
+        avg_reward_array.append(avg_reward)
+
+        print("finished ep {:d} with average reward {:f} in {:d} iters\n".format(e, avg_reward, num_iters))
+
+        num_correct_boxes = correct_boxes(init_state, state, solution)
+        correct_boxes_array.append(num_correct_boxes)
+
+        # train the agent with memory of rewards
+        if num_iters >= 64:
+            agent.replay(64)
+        else:
+            agent.replay(num_iters)
+
+        # save progress periodically    
+        if e !=0 and e % 100 == 0:
+            agent.model.save("my_model")
+            np.savetxt('agent_data.txt',np.array([agent.epsilon, puzz_counter]))
+            num_corr_boxes_save = np.array(correct_boxes_array)
+            avg_reward_save = np.array(avg_reward_array)
+            np.save('num_corr_boxes.npy', num_corr_boxes_save)
+            np.save('avg_reward.npy', avg_reward_save)
+
+# Plot average reward per episode during training
+
+avg_reward_norm = avg_reward_array - np.mean(avg_reward_array)
+episodes_array = np.arange(0, 901)
+bin_size = 20
+
+train =  pd.DataFrame(data=[episodes_array, avg_reward_norm]).T
+train.columns = ['episode', 'avg_reward']
+train['group'] = ((1/bin_size)*train['episode']).astype(int)
+
+temp = train[['avg_reward', 'group']].groupby(['group']).mean()
+temp['group'] = temp.index * bin_size
+temp.reset_index(drop=True)
+
+f = plt.figure(figsize=(5,4))
+plt.plot(temp['group']/9, temp['avg_reward'])
+
+ax=plt.gca()
+plt.xlim([-2,102])
+plt.ylabel('Normalized Average Reward',fontsize=15)
+#f.savefig("avg_reward.jpg",dpi=300)
+
+if evaluate is True:
+
+    num_correct_boxes = []
+
+    for i in range(-1,-101, -1):
+        
+        # choose last 100 puzzles in the dataset for evaluation
+
+        init_state = puzzles[i][0] # Unfinished puzzle
+        solution = puzzles[i][1] # Solution
+        env.base = np.copy(init_state)
+        env.sol = np.copy(solution)
+        init_states_index = get_init_states_index(puzzles_for_test[i][0])
+        env.init_states_index = init_states_index
+        agent.epsilon = 0
+        state = env.reset()
+
+        last_action_count = 0
+        num_iters = 0
+        
+        to_break = False
+        
+        while True:
+            action_index_flat = agent.act(state.reshape(1,-1))
+            action = arr_index_to_action(action_index_flat)
+
+            if num_iters != 0 and np.array_equal(action, last_action):
+                last_action_count += 1
+            else:
+                last_action_count = 0
+
+            next_state, reward, status, _ = env.step(action)
+            state = np.copy(next_state)
+
+            last_action = np.copy(action)
+            print(action)
+
+            if last_action_count == 100 :
+                print('stuck in infinite loop')
+                to_break = True
+            elif np.array_equal(state, solution):
+                print("finished")
+                to_break= True
+            
+            if to_break:
+                num_correct_boxes_ = np.equal(state, env.sol).sum()
+                num_correct_boxes.append(num_correct_boxes_)
+                to_break = False
+                break
+                
+            num_iters += 1
+
+# Plot number of correct boxes during evaluation iterations
+f = plt.figure()
+plt.plot(num_correct_boxes)
+plt.ylabel('Number of correct boxes')
+plt.xlabel('Iterations')
+plt.title('Correct boxes vs iteration')
+plt.show()
+
